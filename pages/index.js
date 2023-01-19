a372f888e726a75807983f0d7e5f2b875277f5fa
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-01-19 13:08:07 +0530

    h

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
 
             <motion.a
               variants={fadeIn('up', 'tween', 0.3, 1)}
-              onClick={() => router.push("/carpets")}
+              onClick={() => router.push("/Carpets")}
               className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 rounded-lg hover:bg-gray-100 shadow-xl focus:ring-4 focus:ring-gray-100 dark:text-[#3B185F] dark:border-gray-700 dark:bg-gray-100 cursor-pointer "
             >
               Browse Product
